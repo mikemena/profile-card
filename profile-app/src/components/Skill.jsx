b7b3e86949e916eb2167867c1c338a50576047ac
tokyo <tokyolabs@protonmail.com>
2024-02-08 07:58:53 -0500

    wip

--- profile-app/src/components/Skill.jsx
+++ profile-app/src/components/Skill.jsx
@@ -2,13 +2,14 @@
 
 function Skill({ skillsObj, isDarkMode }) {
   const { name, imageUrl: image, medalUrl: medal } = skillsObj;
-  const skillPillClass = isDarkMode ? '.skill-pill dark' : '.skill-pill';
+  const skillPillClass = isDarkMode ? 'skill-pill-dark' : 'skill-pill';
+  const skillNameClass = isDarkMode ? 'skill-name-dark' : 'skill-name';
 
   return (
     <div className="skill-container">
-      <div className="skill-pill">
+      <div className={skillPillClass}>
         <img className="medal-icon" src={medal} alt={name} />
-        <h3 className="skill-name">{name}</h3>
+        <h3 className={skillNameClass}>{name}</h3>
         <img className="skill-icon" src={image} alt={name} />
       </div>
     </div>
--- profile-app/src/components/Skillset.jsx
+++ profile-app/src/components/Skillset.jsx
@@ -3,12 +3,9 @@ import skillsData from '../data/skillsData';
 
 function Skillset({ isDarkMode }) {
   const skills = skillsData;
-  const skillsetClass = isDarkMode
-    ? 'skillset-container dark'
-    : 'skillset-container';
 
   return (
-    <div className={skillsetClass}>
+    <div className="skillset-container">
       {skills.map((skill) => (
         <Skill key={skill.id} skillsObj={skill} isDarkMode={isDarkMode} />
       ))}
